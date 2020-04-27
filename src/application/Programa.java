2db9d40059b4944cb1e5fd820c49b8331af59e83
AuRocKbra <4hauroc@gmail.com>
2020-04-27 17:32:41 -0300

    Função Lambda

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -14,10 +14,8 @@ public class Programa {
 		list.add(new Produto("TV",900.00));
 		list.add(new Produto("Notebook",1200.00));
 		list.add(new Produto("Tablet",450.00));
-		
-		Comparator<Produto> comparador =(o1,o2) -> o1.getNome().toUpperCase().compareTo(o2.getNome().toUpperCase());
-		
-		list.sort(comparador);
+				
+		list.sort((o1,o2) -> o1.getNome().toUpperCase().compareTo(o2.getNome().toUpperCase()));
 		
 		for(Produto p : list) {
 			System.out.println(p);
